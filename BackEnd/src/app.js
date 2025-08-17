a9771d13284773978980e4ec09498ea2ef53530e
sarthak <sg166344@gmail.com>
2025-08-17 20:32:04 +0530

    Refactor server initialization: move dotenv config and app.listen to app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -11,4 +11,10 @@ app.use(express.json())
 
 app.use('/ai',aiRoutes)
 
+
+require('dotenv').config()
+app.listen(3000,()=>{
+  console.log("Server is running on http://localhost:3000")
+})
+
 module.exports=app
